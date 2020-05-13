1442b42640c32c4915229e9af261129ad7df7466
sourish <sourishkumarmusib@gmail.com>
2020-05-13 22:31:47 +0530

    Updates

---
+++ precache-manifest.d84c936ecbfb0f08e790c7370daddc4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef302505243f43482bfd853ab51e6ff",
+    "url": "/react-template-grid/index.html"
+  },
+  {
+    "revision": "1c0cb765e4017993d3c1",
+    "url": "/react-template-grid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d4885104c618610969a",
+    "url": "/react-template-grid/static/js/2.e15a8552.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-template-grid/static/js/2.e15a8552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0cb765e4017993d3c1",
+    "url": "/react-template-grid/static/js/main.491e1552.chunk.js"
+  },
+  {
+    "revision": "0000a5bbafa2c2f08dc5",
+    "url": "/react-template-grid/static/js/runtime-main.46ec6119.js"
+  }
+]);
